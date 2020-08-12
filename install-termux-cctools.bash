d38c86a060819de1ffbbb655b5f1a6dc60a6a4d6
Ismael Damião <53145011+ismaeldamiao@users.noreply.github.com>
2020-08-12 15:45:35 -0300

    Add files via upload

---
+++ install-termux-cctools.bash
@@ -0,0 +1,32 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+arch="$(dpkg --print-architecture)"
+cctoolshttp="http://cctools.info/downloads/termux"
+
+# Baixando arquivos
+
+wget -q \
+"${cctoolshttp}/${arch}/binutils-cctools_2.34_${arch}.deb"
+if [ "$?" != "0" ]; then exit 1; fi
+
+wget -q \
+"${cctoolshttp}/${arch}/gcc-cctools_10.1.0_${arch}.deb"
+if [ "$?" != "0" ]; then exit 1; fi
+
+# verificar versão do sdk (__ANDROID_API__)
+sdk="$(getprop ro.build.version.sdk)"
+if [ "$sdk" < "12" ]; then sdk="9"; fi
+if [ "$sdk" == "25" ]; then sdk="24"; fi
+if [ "$sdk" > "26" ]; then sdk="26"; fi
+
+wget -q \
+"${cctoolshttp}/${arch}/ndk-sysroot-cctools-api-${sdk}-${arch}_1.0r15c_all.deb"
+if [ "$?" != "0" ]; then exit 1; fi
+
+dpkg -i *.deb
+if [ "$?" != "0" ]; then exit 1; fi
+
+echo "export PATH=/data/data/com.termux/files/cctools-toolchain/bin:\$PATH"\
+>> $HOME/.bashrc
+
+exit 0
