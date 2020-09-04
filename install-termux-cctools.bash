1f09c63b7b2819199549e2f47a6a54fc72ef38fe
Ismael Damião <53145011+ismaeldamiao@users.noreply.github.com>
2020-09-04 11:44:58 -0300

    Update install-termux-cctools.bash

--- install-termux-cctools.bash
+++ install-termux-cctools.bash
@@ -3,40 +3,32 @@
 # Por Ismael Damião
 # Site: https://ismaeldamiao.github.io/
 # E-mail: ismaellxd@gmail.com
-# Última alteração: 12 de agosto 2020
-
-arch="$(dpkg --print-architecture)"
-cctoolshttp="http://cctools.info/downloads/termux"
+# Última alteração: 4 de setembro de 2020
 
 # Baixando arquivos
 
-wget -q \
-"${cctoolshttp}/${arch}/binutils-cctools_2.34_${arch}.deb"
-if [ "$?" != "0" ]; then exit 1; fi
+if ! wget -O - https://cctools.info/public.key | apt-key add -; then
+   echo ""
+   echo "Precisas instalar o pacote gnupg"
+   echo "apt install -y gnupg"
+   exit 1
+fi
 
-wget -q \
-"${cctoolshttp}/${arch}/gcc-cctools_10.1.0_${arch}.deb"
-if [ "$?" != "0" ]; then exit 1; fi
+echo "deb https://cctools.info termux cctools" > $PREFIX/etc/apt/sources.list.d/cctools.list
+apt update
+apt install -y gcc-cctools
 
 # verificar versão do sdk (__ANDROID_API__)
+arch="$(dpkg --print-architecture)"
 sdk="$(getprop ro.build.version.sdk)"
+
 if [ "$sdk" < "12" ]; then sdk="9"; fi
 if [ "$sdk" == "25" ]; then sdk="24"; fi
 if [ "$sdk" > "26" ]; then sdk="26"; fi
 
-wget -q \
-"${cctoolshttp}/${arch}/ndk-sysroot-cctools-api-${sdk}-${arch}_1.0r15c_all.deb"
-if [ "$?" != "0" ]; then exit 1; fi
+apt install ndk-sysroot-cctools-api-${sdk}-${arch}
 
-# instalando clang
-dpkg -i "binutils-cctools_2.34_${arch}.deb"
-if [ "$?" != "0" ]; then exit 1; fi
-dpkg -i "gcc-cctools_10.1.0_${arch}.deb"
-if [ "$?" != "0" ]; then exit 1; fi
-dpkg -i "ndk-sysroot-cctools-api-${sdk}-${arch}_1.0r15c_all.deb"
-if [ "$?" != "0" ]; then exit 1; fi
-
-echo "export PATH=/data/data/com.termux/files/cctools-toolchain/bin:\$PATH"\
+echo "export \$PREFIX/../cctools-toolchain/bin:\$PATH"\
 >> $HOME/.bashrc
 
 exit 0
